38997d44bac68211370d5505b52606a5b29b5a15
PKAQ <pkaq@msn.com>
2018-06-15 10:29:59 +0800

    可自定义 端口 密码

--- 1kss.sh
+++ 1kss.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-pwd=test123
-read -t 120 -p "输入链接密码：" pwd
+ip=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
+
+read -t 120 -p "输入链接密码(默认：test123)：" pwd
+if [[ ! -n "$pwd" ]] ;then 
+	pwd=test123 
+fi
+
+read -t 120 -p "输入端口(默认：6433)：" port
+if [[ ! -n "$port" ]] ;then 
+	port=6433 
+fi
+
 echo -e "\n"
 # 安装docker
 yum install -y  docker
@@ -10,7 +20,12 @@ systemctl start docker
 # 拉取镜像
 docker pull mritd/shadowsocks
 # 启动容器
-docker run -dt --name ssserver -p 6443:6443 -p 6500:6500/udp mritd/shadowsocks -m "ss-server" -s "-s 0.0.0.0 -p 6443 -m aes-256-cfb -k $pwd --fast-open" -x -e "kcpserver" -k "-t 127.0.0.1:6443 -l :6500 -mode fast2"
+docker run -dt --name ssserver -p $port:6443 -p 6500:6500/udp mritd/shadowsocks -m "ss-server" -s "-s 0.0.0.0 -p 6443 -m aes-256-cfb -k $pwd --fast-open" -x -e "kcpserver" -k "-t 127.0.0.1:6443 -l :6500 -mode fast2"
 
-echo "链接端口: 6443"
-echo "链接密码: $pwd"
+echo "-------------------------------------------------"
+echo ""
+echo " 链接ip: $ip"                                     
+echo " 链接端口: $port"
+echo " 链接密码: $pwd"
+echo ""
+echo "-------------------------------------------------"
